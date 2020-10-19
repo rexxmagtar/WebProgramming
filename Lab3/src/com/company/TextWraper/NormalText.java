394f19423ee11d20c63cc60ab3c990930b39931e
rexx.magtar <rexx.magtar@gmail.com>
2020-10-19 10:59:25 +0300

    web done. Javadoc remains to do

--- Lab3/src/com/company/TextWraper/NormalText.java
+++ Lab3/src/com/company/TextWraper/NormalText.java
@@ -1,7 +1,12 @@
 package com.company.TextWraper;
 
+import com.company.Pair;
+
 import java.util.ArrayList;
 
 public class NormalText {
+
     public ArrayList<Paragraph> paragraphs;
+    public ArrayList<Pair<String,Integer>> codeBlocks;
+
 }
--- Lab3/src/com/company/parsers/NormalTextParser.java
+++ Lab3/src/com/company/parsers/NormalTextParser.java
@@ -1,43 +1,87 @@
 package com.company.parsers;
 
+import com.company.Pair;
 import com.company.TextWraper.NormalText;
 import com.company.TextWraper.Paragraph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
-public  class NormalTextParser {
+public class NormalTextParser {
 
-    public static NormalText ParseNormalText(String text){
+    public static NormalText ParseNormalText(String text) {
 
         NormalText normalText = new NormalText();
 
         ArrayList<Paragraph> paragraphs = new ArrayList<>();
 
+        ArrayList<String> codeBlocks = new ArrayList<>();
+
+        ArrayList<Pair<String, Integer>> codes = new ArrayList<>();
+
+        String codeBlockRegex = "<code>(\\n|.)*<\\/code>";
+
+        Matcher m = Pattern.compile(codeBlockRegex).matcher(text);
+
+        while (m.find()) {
+            codeBlocks.add(m.group());
+        }
+
+        text = text.replaceAll(codeBlockRegex, "<code></code>");
+
+       /* System.out.println("Code wrapped: ");
+        System.out.println(text);*/
+
         ArrayList<String> paragraphStrings = new ArrayList<>();
 
         String regex = "\\n";
 
-       paragraphStrings = new ArrayList<>( Arrays.asList( text.split(regex)));
+        paragraphStrings = new ArrayList<>(Arrays.asList(text.split(regex)));
 
+        int codeBlockNumber = 0;
         for (int i = 0; i < paragraphStrings.size(); i++) {
            /* if(paragraphStrings.get(i).equals("")){
                 continue;
             }*/
-            paragraphs.add(ParagraphParser.ParseParagraph(paragraphStrings.get(i)));
+
+            if (paragraphStrings.get(i).matches(codeBlockRegex)) {
+                codes.add(new Pair<String, Integer>(codeBlocks.get(codeBlockNumber), i));
+                codeBlockNumber++;
+            }
+            else {
+                paragraphs.add(ParagraphParser.ParseParagraph(paragraphStrings.get(i)));
+            }
         }
 
         normalText.paragraphs = paragraphs;
+        normalText.codeBlocks = codes;
 
         return normalText;
     }
 
-    public static String GetString(NormalText text){
+    public static String GetString(NormalText text) {
 
         String result = "";
 
         for (int i = 0; i < text.paragraphs.size(); i++) {
             result += ParagraphParser.GetString(text.paragraphs.get(i)) + "\n";
+            int finalI = i;
+
+           Supplier< Stream<Pair<String,Integer>>> found = ()-> text.codeBlocks.stream().filter(new Predicate<Pair<String, Integer>>() {
+                @Override
+                public boolean test(Pair<String, Integer> stringIntegerPair) {
+                    return stringIntegerPair.getElement1() == finalI +1;
+                }
+            });
+
+            if(found.get().findFirst().isPresent()){
+                result+=found.get().findFirst().get().getElement0()+"\n";
+            }
         }
 
         return result;
